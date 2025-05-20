0e8c390f406f97b3ea1113108e52d5dc04b01a2f
Nibir2405 <nibirislam56@gmail.com>
2025-05-21 01:37:42 +0600

    Add initial migration and model definition for Form

--- donor_reg/models.py
+++ donor_reg/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    blood_group = models.CharField(max_length=80)
+    phone = models.CharField(max_length=80)
+    last_donation = models.DateField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
