793fdc4b7aa7dda18d22c130c5779a4a7ddaedb3
Nibir2405 <nibirislam56@gmail.com>
2025-05-21 23:46:54 +0600

    Implement donor registration form with validation and update index template

---
+++ donor_reg/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    blood_group = forms.CharField(max_length=80)
+    phone = forms.CharField(max_length=80)
+    last_donation = forms.DateField()
+    address = forms.CharField(max_length=100)
--- donor_reg/views.py
+++ donor_reg/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 def index(request):
+    
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            phone = form.cleaned_data["phone"]
+            address = form.cleaned_data["address"]
+            date = form.cleaned_data["last_donation"]
+            blood_group = form.cleaned_data["blood_group"]
+            print(first_name)
     return render(request, "index.html")
