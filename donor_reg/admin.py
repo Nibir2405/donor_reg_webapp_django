7b859cb201b5e415da31f03f81d522730956c06b
Nibir2405 <nibirislam56@gmail.com>
2025-05-23 00:04:12 +0600

    Register Form model with custom admin interface for improved management

--- donor_reg/admin.py
+++ donor_reg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone", "blood_group")
+    search_fields = ("first_name", "blood_group")
+    list_filter =("last_donation", "blood_group")
+    ordering = ("first_name", )
+    #readonly_fields = ("blood_group", )
+admin.site.register(Form, FormAdmin)
