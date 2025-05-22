a2288be6741d4847eb57e9a81d7a2d9a740ce30c
Nibir2405 <nibirislam56@gmail.com>
2025-05-22 16:30:38 +0600

    Add registration success message and save donor data to the database

--- donor_reg/views.py
+++ donor_reg/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Form
+from django.contrib import messages
 
 def index(request):
     
@@ -12,5 +14,9 @@ def index(request):
             address = form.cleaned_data["address"]
             date = form.cleaned_data["last_donation"]
             blood_group = form.cleaned_data["blood_group"]
-            print(first_name)
+            
+            Form.objects.create(first_name=first_name, last_name=last_name, blood_group=blood_group,
+                         phone=phone, last_donation=date, location=address)
+            
+            messages.success(request, "Registration Successfull")
     return render(request, "index.html")
